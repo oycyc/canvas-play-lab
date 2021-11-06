a2df365f373b112ca99da05fc64e73e47e5b5e29
yangciou <me@yangciou.com>
2021-11-06 14:18:28 -0400

    update

--- progress-bar/script.js
+++ progress-bar/script.js
@@ -4,11 +4,7 @@ canvas.width = 500;
 const ctx = canvas.getContext("2d");
 
 
-var grd = ctx.createLinearGradient(50, 50, 450, 50)
-// light blue
-grd.addColorStop(0, '#8EC5FC');
-// dark blue
-grd.addColorStop(1, '#E0C3FC');
+
 
 
 
@@ -20,7 +16,7 @@ grd.addColorStop(1, '#E0C3FC');
 // ctx.lineTo(450 , 50);
 // ctx.stroke();
 
-
+let frame = 0;
 
 const progressBars = [];
 
@@ -34,30 +30,42 @@ class progressBar {
     }
 
     draw() {
-        console.log("drawing");
-        ctx.strokeStyle = grd;
-        ctx.beginPath();
-        ctx.moveTo(this.startX, this.startY);
-        ctx.lineWidth = 15;
-        ctx.lineCap = "round";
-        ctx.lineTo(this.startX + this.currentValue, this.startY);
-        ctx.stroke();
-
-        if (this.currentValue < this.endValue) this.currentValue++;
+        if (frame % 1 === 0) {
+            var grd = ctx.createLinearGradient(this.startX, this.startY, this.startX + this.currentValue, this.startY);
+            // light blue
+            grd.addColorStop(0, '#8EC5FC');
+            // dark blue
+            grd.addColorStop(1, '#E0C3FC');
+    
+            console.log("drawing");
+            ctx.strokeStyle = grd;
+            ctx.beginPath();
+            ctx.moveTo(this.startX, this.startY);
+            ctx.lineWidth = 15;
+            ctx.lineCap = "round";
+            ctx.lineTo(this.startX + this.currentValue, this.startY);
+            ctx.stroke();
+    
+            if (this.currentValue < this.endValue) this.currentValue++;
+            console.log(this.currentValue)
+            console.log("---")
+            console.log(this.endValue)
+        }
     }
 
 
 }
 
-progressBars.push(new progressBar(50, 50, 150))
+progressBars.push(new progressBar(25, 50, 450))
+progressBars.push(new progressBar(25, 150, 450))
 
 // progressBars[0].draw();
 function animate() {
-    console.log("animating");
-    // for (let i = 0; i < progressBars; i++) {
-    //     progressBars[i].draw();
-    // }
-    progressBars[0].draw();
+    frame++;
+    for (let i = 0; i < progressBars.length; i++) {
+        progressBars[i].draw();
+    }
+
     requestAnimationFrame(animate);
 }
 animate();
